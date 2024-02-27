4e5feee3d8afed5ede282a7cbd73ca66bfc266d8
Leonardo Gatti <leonardogattidev@gmail.com>
2024-02-27 03:00:31 -0300

    stage 4

--- app/server.go
+++ app/server.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
-
-var connNum = 0
 
 func main() {
 	protocol := "tcp"
@@ -28,7 +25,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			break
 		}
-		connNum += 1
 
 		go HandleFunction(conn)
 	}
@@ -36,26 +32,27 @@ func main() {
 
 func HandleFunction(conn net.Conn) {
 	defer conn.Close()
+
 	var buf [256]byte
 
-	fmt.Printf("Processing connection #%v\n", connNum)
-	time.Sleep(time.Second)
-
 	for {
-		n, err := conn.Read(buf[:])
+		_, err := conn.Read(buf[:])
 		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
 			fmt.Println("Failed to read data from connection.\nError:", err)
 			break
 		}
-		received := buf[:n]
-		fmt.Printf("Received: %v bytes\n```\n%v```\n", n, string(received))
+		// received := buf[:n]
+		// fmt.Printf("Received: %v bytes\n```\n%v```\n", n, string(received))
 
 		response := []byte("+PONG\r\n")
-		n, err = conn.Write(response)
+		_, err = conn.Write(response)
 		if err != nil {
 			fmt.Println("Failed to write to connection.\nError:", err)
 			break
 		}
-		fmt.Printf("Sent: %v bytes\n```\n%v```\n", n, string(response))
+		// fmt.Printf("Sent: %v bytes\n```\n%v```\n", n, string(response))
 	}
 }
