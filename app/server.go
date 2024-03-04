68280d518d23bb578a5d224beffc83cd41f8561e
Leonardo Gatti <leonardogattidev@gmail.com>
2024-03-04 00:09:55 -0300

    stage 5

--- app/server.go
+++ app/server.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
-	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	protocol := "tcp"
-	ip := "0.0.0.0"
-	port := 6379
-	host := fmt.Sprint(ip, ":", port)
+	run()
+}
 
-	listener, err := net.Listen(protocol, host)
+const DEFAULT_REDIS_PORT = 6379
+
+func run() {
+	port := DEFAULT_REDIS_PORT
+	host := fmt.Sprintf(":%v", port)
+	listener, err := net.Listen("tcp", host)
 	if err != nil {
 		fmt.Println("Failed to bind to port", port, "\nError:", err)
-		os.Exit(1)
+		return
 	}
 	defer listener.Close()
 
+	fmt.Printf("Listening on port %v\n", port)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -26,33 +32,78 @@ func main() {
 			break
 		}
 
-		go HandleFunction(conn)
+		go handleFunction(conn)
 	}
 }
 
-func HandleFunction(conn net.Conn) {
+func handleFunction(conn net.Conn) {
 	defer conn.Close()
 
 	var buf [256]byte
 
 	for {
-		_, err := conn.Read(buf[:])
+		n, err := conn.Read(buf[:])
 		if err != nil {
-			if err.Error() == "EOF" {
+			if err == io.EOF {
 				break
 			}
 			fmt.Println("Failed to read data from connection.\nError:", err)
 			break
 		}
-		// received := buf[:n]
-		// fmt.Printf("Received: %v bytes\n```\n%v```\n", n, string(received))
+		received := buf[:n]
+		fmt.Printf("Received: %v bytes\n```\n%v```\n", n, string(received))
 
-		response := []byte("+PONG\r\n")
-		_, err = conn.Write(response)
+		var response string
+
+		args, errMsg := parseRedisCommand(string(received))
+		if errMsg != "" {
+			response = errMsg
+		}
+		switch strings.ToUpper(args[0]) {
+		case "ECHO":
+			response = makeResponse(args[1])
+
+		case "PING":
+			response = makeResponse("PONG")
+
+		}
+
+		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Failed to write to connection.\nError:", err)
 			break
 		}
-		// fmt.Printf("Sent: %v bytes\n```\n%v```\n", n, string(response))
 	}
+}
+
+func parseRedisCommand(input string) ([]string, string) {
+	lines := strings.Split(strings.TrimSuffix(input, "\r\n"), "\r\n")
+
+	linesLen := len(lines)
+
+	for i := 0; i < linesLen; i++ {
+		line := lines[i]
+		fmt.Printf("[%v] %s\n", i, line)
+
+	}
+
+	var args []string
+
+	switch lines[0][0] {
+	default:
+		return nil, "Invalid Redis command, root element should be \"*\"(array)"
+	case '*':
+		argCount, _ := strconv.Atoi(lines[0][1:])
+
+		// TODO: parse each argument in the array
+		for i := 0; i < argCount; i++ {
+			args = append(args, lines[2+i*2])
+		}
+		fmt.Println("Args:", args)
+	}
+	return args, ""
+}
+
+func makeResponse(s string) string {
+	return fmt.Sprintf("+%s\r\n", s)
 }
